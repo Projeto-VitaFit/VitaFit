ed29dab9c02af9530fea573872d6b6d3c1852136
Andreza Alves <drezlves@gmail.com>
2020-06-15 20:15:33 -0300

    Javascript

--- javascript/objetivo.js
+++ javascript/objetivo.js
@@ -1,54 +1,6 @@
-function populateUFs () {
-    const ufSelect = document.querySelector("select[name=uf]")
-    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome")
-    .then( res => res.json() )
-    .then( states => {
-
-        for( const state of states ) {
-            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
-        }
- 
-    })
-}
-
-populateUFs()
-
-function getCities(event) {
-    const citySelect = document.querySelector("[name=city]")
-    const stateInput = document.querySelector("[name=state]")
-    
-    const ufValue = event.target.value 
-
-    const indexOfSelectedState = event.target.selectedIndex
-    stateInput.value = event.target.options[indexOfSelectedState].text
-
-    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
-
-
-    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
-    citySelect.disabled = true
-
-    fetch(url)
-    .then( res => res.json() )
-    .then( cities => {
-
-        for( const city of cities ) {
-            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
-        }
- 
-        citySelect.disabled = false
-
-    })
-}
-
-// ouvidor de eventos ao Uf / mudei
-document
-    .querySelector("select[name=uf]")
-    .addEventListener("change", getCities)
-
-    // items de coleta
+// items de coleta sexo
     // pegar todos os li's
-    const itemsToCollect = document.querySelectorAll(".items-grid li")
+    const itemsToCollect = document.querySelectorAll(".items-grid3 li")
     // add o ouvidor de eventos / click
     for (const item of itemsToCollect) {
         item.addEventListener("click", handleSelectItem)
@@ -110,6 +62,4 @@ document
         
     }
 
-
-
-    
+  
